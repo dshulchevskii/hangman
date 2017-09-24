7b2c0def578cd85c45c4230a2e384d187348f135
Dmitriy Shulchevskii <dshulchevskii@yandex.ru>
2017-09-25 01:28:34 +0300

    Add game

---
+++ hangman/__init__.py
@@ -0,0 +1,11 @@
+from .game import Hangman
+from random import choice
+
+
+__version__ = '0.0.0'
+
+if __name__ == '__main__':
+    words = ['hello', 'world', 'python', 'ruby']
+    word = choice(words)
+    play = Hangman(word)
+    play.run()
---
+++ hangman/game.py
@@ -0,0 +1,33 @@
+max_mistakes = 5
+
+
+class Hangman():
+
+    def __init__(self, word):
+        self.word = word
+        self.mistakes = 0
+        self.letters = []
+
+    def open_word(self):
+        return ''.join(l if l in self.letters else '*' for l in self.word)
+
+    def step(self):
+        print('Guess a letter:')
+        letter = str(input())
+        self.letters.append(letter)
+        if letter in self.word:
+            print('Hit!')
+        else:
+            self.mistakes += 1
+            print('Missed, mistake %d out of %d.' % (self.mistakes,
+                                                     max_mistakes))
+        print()
+        print("The word: %s" % self.open_word())
+
+    def run(self):
+        while self.mistakes < max_mistakes:
+            self.step()
+            if self.open_word() == self.word:
+                print('You won')
+                return
+        print('You lost!')
